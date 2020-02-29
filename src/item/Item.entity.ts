b7b98301b57a650f9290a28517c79da51ad32f27
maorRoz <andory@post.bgu.ac.il>
2020-02-29 04:07:41 +0200

    imporoved item properties declarations

--- src/item/Item.entity.ts
+++ src/item/Item.entity.ts
@@ -6,28 +6,42 @@ import {
   IsBoolean,
   Min,
   Max,
-  IsOptional
+  IsOptional,
+  Matches
 } from 'class-validator';
+
+enum ItemSlot {
+  OneHanded = 'one-handed',
+  TwoHanded = 'two-handed',
+  Head = 'head',
+  Chest = 'chest',
+  Boots = 'boots',
+  Consumable = 'consumable'
+}
+
+const ItemProsperity = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
 
 @Entity()
 export class Item {
   @IsInt()
   @ApiProperty({ required: true, example: 1 })
-  @PrimaryColumn()
+  @PrimaryColumn({ type: 'integer' })
   id: number;
 
   @IsString()
   @ApiProperty({ required: true, example: 'Boots of Striding' })
-  @Column({ unique: true })
+  @Column({ type: 'text', unique: true })
   name: string;
 
-  @ApiProperty({ required: true, example: 'one-handed' })
-  @Column()
-  slot: 'one-handed' | 'two-handed' | 'head' | 'chest' | 'boots' | 'consumable';
+  @Matches(/^(one-handed|two-handed|head|chest|boots|consumable)$/)
+  @ApiProperty({ enum: ItemSlot, required: true, example: 'one-handed' })
+  @Column({ type: 'enum', enum: ItemSlot })
+  slot: ItemSlot;
 
   @IsString()
+  //@IsUrl()
   @ApiProperty({ required: true, example: '...' })
-  @Column()
+  @Column({ type: 'text' })
   image: string;
 
   @IsOptional()
@@ -35,12 +49,12 @@ export class Item {
   @Min(1)
   @Max(9)
   @ApiProperty({ example: 1 })
-  @Column({ nullable: true })
-  prosperity?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+  @Column({ type: 'enum', enum: ItemProsperity, nullable: true })
+  prosperity?: typeof ItemProsperity[number];
 
   @IsOptional()
   @IsBoolean()
-  @Column({ nullable: true })
+  @Column({ type: 'boolean', nullable: true })
   @ApiProperty({ example: false })
   rare?: boolean;
 }
