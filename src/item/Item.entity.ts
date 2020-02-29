c759874701a5fe5d136abae4b816774b07144573
maorRoz <andory@post.bgu.ac.il>
2020-02-29 13:59:25 +0200

    improved item type doc and validation, added quantity field

--- src/item/Item.entity.ts
+++ src/item/Item.entity.ts
@@ -3,28 +3,30 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   IsInt,
   IsString,
-  IsBoolean,
   Min,
   Max,
   IsOptional,
-  Matches,
-  IsUrl
+  IsUrl,
+  IsIn
 } from 'class-validator';
 
 enum ItemSlot {
-  OneHanded = 'one-handed',
-  TwoHanded = 'two-handed',
-  Head = 'head',
-  Chest = 'chest',
-  Boots = 'boots',
-  Consumable = 'consumable'
+  OneHand = 'One Hand',
+  TwoHands = 'Two Hands',
+  Head = 'Head',
+  Body = 'Body',
+  Legs = 'Legs',
+  SmallItem = 'Small Item'
 }
+
+const ItemQuantity = [1, 2, 4] as const;
 
 const ItemProsperity = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
 
 @Entity()
 export class Item {
   @IsInt()
+  @Min(1)
   @ApiProperty({ required: true, example: 1 })
   @PrimaryColumn({ type: 'integer' })
   id: number;
@@ -34,8 +36,8 @@ export class Item {
   @Column({ type: 'text', unique: true })
   name: string;
 
-  @Matches(/^(one-handed|two-handed|head|chest|boots|consumable)$/)
-  @ApiProperty({ enum: ItemSlot, required: true, example: 'boots' })
+  @IsIn(Object.values(ItemSlot))
+  @ApiProperty({ enum: ItemSlot, required: true, example: 'Legs' })
   @Column({ type: 'enum', enum: ItemSlot })
   slot: ItemSlot;
 
@@ -55,17 +57,16 @@ export class Item {
   @Column({ type: 'integer' })
   price: number;
 
+  @IsIn(Object.values(ItemQuantity))
+  @ApiProperty({ enum: ItemQuantity, required: true, example: 2 })
+  @Column({ type: 'integer' })
+  quantity: typeof ItemQuantity[number];
+
   @IsOptional()
   @IsInt()
   @Min(1)
   @Max(9)
-  @ApiProperty({ example: 1 })
+  @ApiProperty({ enum: ItemProsperity, example: 1 })
   @Column({ type: 'enum', enum: ItemProsperity, nullable: true })
   prosperity?: typeof ItemProsperity[number];
-
-  @IsOptional()
-  @IsBoolean()
-  @Column({ type: 'boolean', nullable: true })
-  @ApiProperty({ example: false })
-  rare?: boolean;
 }
